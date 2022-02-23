b1bd6640a80737609e2eb976edc0d571f275debc
Marcus <marcus.lyz98@gmail.com>
2022-02-23 14:15:19 +0800

    Add Category,Deadline,Event,Task

--- DIP/App.js
+++ DIP/App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Tabs from './navigation/tabs'
 import TopBar from './navigation/TopBar';
-
+// import AddCategory from './AddCategory';
 
 const App = () => {
   return(
