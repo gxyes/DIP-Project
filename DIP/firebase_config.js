597cce4768539ef8cb05b0813ad8895e59e0922b
nemopotatoes <jesgjx@gmail.com>
2022-04-18 11:29:28 +0800

    quota exceeded - switched database

--- DIP/firebase_config.js
+++ DIP/firebase_config.js
@@ -5,26 +5,26 @@ import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
 
 // Your web app's Firebase configuration
 // Jessie database
-const firebaseConfig = {
-  apiKey: "AIzaSyCUlO8uBXhRh-8VadkcQwY7TrNEskKJ5XI",
-  authDomain: "dipcalendarsafe.firebaseapp.com",
-  projectId: "dipcalendarsafe",
-  storageBucket: "dipcalendarsafe.appspot.com",
-  messagingSenderId: "212189152592",
-  appId: "1:212189152592:web:91af02079d5d7b38bf9855",
-  measurementId: "G-8MTKRWTFNB"
-  };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCUlO8uBXhRh-8VadkcQwY7TrNEskKJ5XI",
+//   authDomain: "dipcalendarsafe.firebaseapp.com",
+//   projectId: "dipcalendarsafe",
+//   storageBucket: "dipcalendarsafe.appspot.com",
+//   messagingSenderId: "212189152592",
+//   appId: "1:212189152592:web:91af02079d5d7b38bf9855",
+//   measurementId: "G-8MTKRWTFNB"
+//   };
 
 // Marcus database
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAp_x4Q-bDdgY2isivuOi8xtRMVYm9zQOQ",
-//   authDomain: "dipcalender.firebaseapp.com",
-//   projectId: "dipcalender",
-//   storageBucket: "dipcalender.appspot.com",
-//   messagingSenderId: "117720083664",
-//   appId: "1:117720083664:web:8bd7f53cb122e1a24b02d5",
-//   measurementId: "G-WF4Z0W73YH"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAp_x4Q-bDdgY2isivuOi8xtRMVYm9zQOQ",
+  authDomain: "dipcalender.firebaseapp.com",
+  projectId: "dipcalender",
+  storageBucket: "dipcalender.appspot.com",
+  messagingSenderId: "117720083664",
+  appId: "1:117720083664:web:8bd7f53cb122e1a24b02d5",
+  measurementId: "G-WF4Z0W73YH"
+};
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
